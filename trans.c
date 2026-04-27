8f3d0dbb3187de1900f346c4f3cdc3a2bc02d078
Sanajy <sri sanjai.n@soi.kgkite.ac.in>
2026-04-27 07:47:09 +0530

    Display All Accounts

--- trans.c
+++ trans.c
@@ -141,6 +141,25 @@ void updateRecord(FILE *fPtr)
     } // end else
 } // end function updateRecord
 
+void displayAll(FILE *fPtr)
+{
+    struct clientData client = {0, "", "", 0.0};
+
+    rewind(fPtr);
+
+    printf("\n%-6s%-16s%-11s%10s\n", "Acct", "Last Name", "First Name", "Balance");
+
+    while (fread(&client, sizeof(struct clientData), 1, fPtr))
+    {
+        if (client.acctNum != 0)
+        {
+            printf("%-6d%-16s%-11s%10.2f\n",
+                   client.acctNum, client.lastName,
+                   client.firstName, client.balance);
+        }
+    }
+}
+
 // delete an existing record
 void deleteRecord(FILE *fPtr)
 {
@@ -216,7 +235,8 @@ unsigned int enterChoice(void)
                  "3 - add a new account\n"
                  "4 - delete an account\n"
                  "5 - display an account\n"
-                 "6 - end program\n? ");
+                 "6 - display all accounts\n"
+                 "7 - end program\n");
 
     scanf("%u", &menuChoice); // receive choice from user
     return menuChoice;
